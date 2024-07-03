4fb2246e03236c3f913564de22f234d47b77a707
HP <HP@LAPTOP-7HL8C2SN>
2024-07-03 18:32:05 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Master",
+                DateOfBirth = new DateTime(2000,07,21)
+            };
+            return View(data);
+        }
+    }
+}
