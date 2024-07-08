ec97b1a2cae15b4b2a0c59c6df65378e5e5cc72a
Ankit Choudhary <ankitnature2000@gmail.com>
2024-07-08 15:40:22 +0530

    Completed Leave Management Module

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName ="nvarchar(150)")]
+        public int NumberOfDays { get; set; }
+    }
+}
